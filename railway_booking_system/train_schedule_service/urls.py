725c7302464e2aa0c811c6f677fd156855eb200c
Tharusha Heshan Black <163631097+Tharusha-Black@users.noreply.github.com>
2024-04-08 18:33:50 +0530

    Update urls.py

--- railway_booking_system/train_schedule_service/urls.py
+++ railway_booking_system/train_schedule_service/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('edit_train/<int:train_id>/', views.edit_train, name='edit_train'),
     path('delete_train/<int:train_id>/', views.delete_train, name='delete_train'),
-     path('edit_schedule/<int:schedule_id>/', views.edit_schedule, name='edit_schedule'),
+    path('edit_schedule/<int:schedule_id>/', views.edit_schedule, name='edit_schedule'),
     path('delete_schedule/<int:schedule_id>/', views.delete_schedule, name='delete_schedule'),
 
 ]
