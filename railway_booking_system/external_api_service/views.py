2f0271264ddaa2ce6b900aed0a9cb6cf68433692
Tharusha Heshan Black <163631097+Tharusha-Black@users.noreply.github.com>
2024-04-09 09:53:36 +0530

    Update views.py

--- railway_booking_system/external_api_service/views.py
+++ railway_booking_system/external_api_service/views.py
@@ -8,14 +8,24 @@ class TrainListView(APIView):
     renderer_classes = [JSONRenderer]
 
     def get(self, request):
+        # Retrieve all trains from the database
         trains = Train.objects.all()
+
+        # Serialize the trains data
         serializer = TrainSerializer(trains, many=True)
+
+        # Return the serialized data as a JSON response
         return Response(serializer.data)
 
 class ScheduleListView(APIView):
     renderer_classes = [JSONRenderer]
 
     def get(self, request):
+        # Retrieve all train schedules from the database
         schedules = TrainSchedule.objects.all()
+
+        # Serialize the schedules data
         serializer = TrainScheduleSerializer(schedules, many=True)
+
+        # Return the serialized data as a JSON response
         return Response(serializer.data)
