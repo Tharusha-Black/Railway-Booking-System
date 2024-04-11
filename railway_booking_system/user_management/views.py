8673bfc21d2ffd5f1856fa2b1b488cc7903d03ff
Tharusha Heshan Black <163631097+Tharusha-Black@users.noreply.github.com>
2024-04-11 11:31:26 +0530

    Update views.py

--- railway_booking_system/user_management/views.py
+++ railway_booking_system/user_management/views.py
@@ -1,42 +1,41 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout 
-from .forms import SignupForm, LoginForm
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.views import View
+from django.utils.decorators import method_decorator
+from .forms import SignupForm, LoginForm
 
+class IndexView(View):
+    def get(self, request):
+        return render(request, 'user_management/index.html')
 
-# Create your views here.
-# Home page
-def index(request):
-    return render(request, 'user_management/index.html')
-
-# signup page
-def user_signup(request):
-    if request.method == 'POST':
+class UserSignupView(View):
+    def get(self, request):
+        form = SignupForm()
+        return render(request, 'user_management/sign_up.html', {'form': form})
+    
+    def post(self, request):
         form = SignupForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('login')
-    else:
-        form = SignupForm()
-    return render(request, 'user_management/sign_up.html', {'form': form})
+        return render(request, 'user_management/sign_up.html', {'form': form})
 
-# login page
-def user_login(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
+class UserLoginView(View):
+    def get(self, request):
+        form = LoginForm()
+        return render(request, 'user_management/login.html', {'form': form})
+    
+    def post(self, request):
+        form = LoginForm(request, data=request.POST)
         if form.is_valid():
             user = form.get_user()
-            login(request, user)  # Log in the user
-            if user.is_superuser:
-                return redirect('dashboard')  # Redirect superusers to dashboard
-            else:
-                return redirect('search_trains')  # Redirect regular users to home
-    else:
-        form = AuthenticationForm()
-    return render(request, 'user_management/login.html', {'form': form})
+            login(request, user)
+            return redirect('dashboard') if user.is_superuser else redirect('search_trains')
+        return render(request, 'user_management/login.html', {'form': form})
 
-# logout page
-def user_logout(request):
-    logout(request)
-    return redirect('home')
+@method_decorator(login_required, name='dispatch')
+class UserLogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('home')
