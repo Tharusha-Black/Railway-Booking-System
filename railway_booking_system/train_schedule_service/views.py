7f9b87ab316a3c7e852e0838d4d6b8013d41e088
Tharusha Heshan Black <163631097+Tharusha-Black@users.noreply.github.com>
2024-04-11 11:29:36 +0530

    Update views.py

--- railway_booking_system/train_schedule_service/views.py
+++ railway_booking_system/train_schedule_service/views.py
@@ -1,75 +1,88 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import user_passes_test
-from .forms import TrainForm, TrainScheduleForm
 from django.contrib import messages
+from django.views import View
+from .forms import TrainForm, TrainScheduleForm
 from .models import Train, TrainSchedule
-from django.shortcuts import get_object_or_404
+from django.utils.decorators import method_decorator
 
-def dashboard(request):
-    trains = Train.objects.all()
-    schedules = TrainSchedule.objects.all()
-    return render(request, 'train_schedule_service/dashboard.html', {'trains': trains, 'schedules': schedules})
 
-def edit_train(request, train_id):
-    train = get_object_or_404(Train, pk=train_id)
-    if request.method == 'POST':
+class DashboardView(View):
+    def get(self, request):
+        trains = Train.objects.all()
+        schedules = TrainSchedule.objects.all()
+        return render(request, 'train_schedule_service/dashboard.html', {'trains': trains, 'schedules': schedules})
+
+class EditTrainView(View):
+    def get(self, request, train_id):
+        train = get_object_or_404(Train, pk=train_id)
+        form = TrainForm(instance=train)
+        return render(request, 'train_schedule_service/train_add_form.html', {'form': form})
+
+    def post(self, request, train_id):
+        train = get_object_or_404(Train, pk=train_id)
         form = TrainForm(request.POST, instance=train)
         if form.is_valid():
             form.save()
             return redirect('dashboard')
-    else:
-        form = TrainForm(instance=train)
-    return render(request, 'train_schedule_service/train_add_form.html', {'form': form})
+        return render(request, 'train_schedule_service/train_add_form.html', {'form': form})
 
-def delete_train(request, train_id):
-    train = get_object_or_404(Train, pk=train_id)
-    if request.method == 'POST':
+class DeleteTrainView(View):
+    def post(self, request, train_id):
+        train = get_object_or_404(Train, pk=train_id)
         train.delete()
         return redirect('dashboard')
-    return render(request, 'train_schedule_service/train_confirm_delete.html', {'train': train})
 
-@user_passes_test(lambda u: u.is_superuser)
-def add_train(request):
-    if request.method == 'POST':
+class AddTrainView(View):
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def get(self, request):
+        form = TrainForm()
+        return render(request, 'train_schedule_service/train_add_form.html', {'form': form})
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def post(self, request):
         form = TrainForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'Train successfully added!')
-            return redirect('dashboard')  # Redirect to the dashboard with a success message
-    else:
-        form = TrainForm()
-    return render(request, 'train_schedule_service/train_add_form.html', {'form': form})
+            return redirect('dashboard')
+        return render(request, 'train_schedule_service/train_add_form.html', {'form': form})
 
-@user_passes_test(lambda u: u.is_superuser)
-def schedule_train(request):
-    if request.method == 'POST':
+class ScheduleTrainView(View):
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def get(self, request):
+        form = TrainScheduleForm()
+        return render(request, 'train_schedule_service/schedule_form.html', {'form': form})
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def post(self, request):
         form = TrainScheduleForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('dashboard')  # Redirect to a success page
-    else:
-        form = TrainScheduleForm()
-    return render(request, 'train_schedule_service/schedule_form.html', {'form': form})
+            return redirect('dashboard')
+        return render(request, 'train_schedule_service/schedule_form.html', {'form': form})
 
-@user_passes_test(lambda u: u.is_superuser)
-def edit_schedule(request, schedule_id):
-    schedule = get_object_or_404(TrainSchedule, pk=schedule_id)
-    if request.method == 'POST':
+class EditScheduleView(View):
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def get(self, request, schedule_id):
+        schedule = get_object_or_404(TrainSchedule, pk=schedule_id)
+        form = TrainScheduleForm(instance=schedule)
+        return render(request, 'train_schedule_service/schedule_form.html', {'form': form})
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def post(self, request, schedule_id):
+        schedule = get_object_or_404(TrainSchedule, pk=schedule_id)
         form = TrainScheduleForm(request.POST, instance=schedule)
         if form.is_valid():
             form.save()
             messages.success(request, 'Schedule successfully updated!')
             return redirect('dashboard')
-    else:
-        form = TrainScheduleForm(instance=schedule)
-    return render(request, 'train_schedule_service/schedule_form.html', {'form': form})
+        return render(request, 'train_schedule_service/schedule_form.html', {'form': form})
 
-# View to delete a train schedule
-@user_passes_test(lambda u: u.is_superuser)
-def delete_schedule(request, schedule_id):
-    schedule = get_object_or_404(TrainSchedule, pk=schedule_id)
-    if request.method == 'POST':
+class DeleteScheduleView(View):
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def post(self, request, schedule_id):
+        schedule = get_object_or_404(TrainSchedule, pk=schedule_id)
         schedule.delete()
         messages.success(request, 'Schedule successfully deleted!')
         return redirect('dashboard')
-    return render(request, 'train_schedule_service/schedule_confirm_delete.html', {'schedule': schedule})
